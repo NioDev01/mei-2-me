c3641017b6d964d94ce77ef26bd833023770f7c5
SasshaSabrina <146472179+SasshaSabrina@users.noreply.github.com>
2025-06-14 11:56:56 -0300

    Create Treinamento.tsx

---
+++ client/src/pages/Treinamento.tsx
@@ -0,0 +1,72 @@
+ import { Button } from "@/components/ui/button";
+ import {Input} from "@/components/ui/input";
+ import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+ } from "@/components/ui/form";
+ import { zodResolver } from "@hookform/resolvers/zod";
+ import { useForm } from "react-hook-form";
+ import { z } from "zod";
+ import axios from "axios";
+ import { toast, Toaster } from "sonner";
+
+  export function Treinamento() {
+
+    const form = useForm<z.infer<typeof EmailFormSchema>>({
+    resolver: zodResolver(EmailFormSchema),
+    defaultValues: {
+    email: '',
+  },
+ });
+
+ async function onSubmit(data: z.infer<typeof EmailFormSchema>){
+  await axios.post(`${import.meta.env.VITE_API_URL}/email`, {
+    conteudo: data.email,
+  })
+  .then((response) => {
+    toast.success(`Email enviado: ${response.data.conteudo}`);
+    form.reset();
+  })
+  .catch((error) => {
+    console.error(error);
+    toast.error('Erro ao enviar seu email.');
+  });
+}
+
+    return (  
+     <div className="max-w-xl mx-auto mt-10 space-y-10">
+        <Toaster position="top-center" />
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+                <FormField
+                    control={form.control}
+                    name='email'
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                <Input type='email' placeholder='Digite seu email' {...field} />
+                            </FormControl>
+                            <FormDescription>
+                                Este email será enviado para o servidor.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type='submit' variant={"outline"}>Enviar</Button>
+            </form>
+        </Form>
+    </div>
+  )}
+
+  const EmailFormSchema = z.object({
+  email: z.string().email({
+    message: "Por favor, insira um email válido.",
+  }),
+ });
