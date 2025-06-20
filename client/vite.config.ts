4d4f19f8d03b816f3c8e6161b0c6526f776e08b0
Antonio Sales <antoniosalesdev@gmail.com>
2025-06-19 23:34:05 -0300

    deploy: preparação client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,4 +11,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'dist',
+    sourcemap: process.env.NODE_ENV !== 'production',
+  },
 })
