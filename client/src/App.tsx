b9c11db26ce7d2f735346ab7947a9ef9ae8511be
Antonio Sales <antoniosalesdev@gmail.com>
2025-06-29 17:53:29 -0300

    fix: update image source path in App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <div>
       {/* Navegação */}
       <nav className="fixed z-50 top-0 left-0 flex items-center space-x-4 mb-6 bg-sidebar-primary w-full h-1/10">
-        <Link to="/"><img className='mx-8 h-1/3 w-1/3' src='public/mei2mew.png'></img></Link>
+        <Link to="/"><img className='mx-8 h-1/3 w-1/3' src='/mei2mew.png'></img></Link>
         <Link to="/mensagens" className="text-background hover:underline">Mensagens</Link>
       </nav>
 
