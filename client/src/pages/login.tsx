ac12b0bcf33e0f9c93f47f4d2017700dedb55666
LucasDuarte42 <lucasduarte0227@gmail.com>
2025-07-04 14:06:10 -0300

    feat: ajuste tela login

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -4,7 +4,6 @@
 // import { Input } from "@/components/ui/input";
 // import { Button } from "@/components/ui/button";
 // import { Label } from "@/components/ui/label";
-import { GalleryVerticalEnd } from "lucide-react"
 import { LoginForm } from "@/features/login-form"
 import mei2me from '@/assets/mei2me.png';
 
@@ -15,15 +14,9 @@ return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
-            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-              <GalleryVerticalEnd className="size-4" />
-            </div>
-            Acme Inc.
-          </a>
         </div>
         <div className="flex flex-1 items-center justify-center">
-          <div className="w-full max-w-xs">
+          <div className="w-full max-w-lg">
             <LoginForm />
           </div>
         </div>
@@ -32,10 +25,11 @@ return (
         <img
           src={mei2me}
           alt="Image"
-          className="absolute inset-0 h-full w-full object-cover "
+          className=" absolute inset-0 h-[500px] w-[500px] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
         />
       </div>
     </div>
+    
   )
 }
  
