a52b57890794433d9221a54d5c4cd9da8ca5e343
Antonio Sales <antoniosalesdev@gmail.com>
2025-07-14 14:58:52 -0300

    feat: add route for diagnostic page

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import { Home } from '@/pages/Home';
 import { Mensagens } from '@/pages/Mensagens';
 import { Login} from '@/pages/login';
+import { DiaInicial } from '@/pages/diagnostico-inicial';
 
 export function AppRoutes() {
   return (
@@ -9,6 +10,7 @@ export function AppRoutes() {
       <Route path="/" element={<Home />} />
       <Route path="/mensagens" element={<Mensagens />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/diagnostico" element={<DiaInicial />} />
     </Routes>
   );
 }
