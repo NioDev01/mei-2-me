0f3bd2fa3c80b403baa208981e6f04e5827ff2c8
LucasDuarte42 <lucasduarte0227@gmail.com>
2025-07-28 19:55:14 -0300

    Update index.tsx

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -3,6 +3,8 @@ import { Home } from '@/pages/Home';
 import { Mensagens } from '@/pages/Mensagens';
 import { Login} from '@/pages/login';
 import { DiaInicial } from '@/pages/DiagnosticoInicial';
+import { RedefinirSenha } from '@/pages/RedefinirSenha';
+import { RecuperarSenha } from '@/pages/RecuperarSenha';
 
 
 export function AppRoutes() {
@@ -12,6 +14,9 @@ export function AppRoutes() {
       <Route path="/mensagens" element={<Mensagens />} />
       <Route path="/login" element={<Login />} />
       <Route path="/diagnostico" element={<DiaInicial />} />
+      <Route path="/redefinirsenha" element={<RedefinirSenha />} />
+      <Route path="/recuperarsenha" element={<RecuperarSenha />} />
+
     </Routes>
   );
 }
