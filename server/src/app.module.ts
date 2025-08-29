23283a45a22760928c6a938600fd967e8e05edcc
Luana Silva <luanasilva.developer@gmail.com>
2025-08-28 21:30:09 -0300

    feat: adiciona módulos

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { MessageModule } from './modules/message/message.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { ReceitawsApiService } from './integrations/receitaws-api/receitaws-api.service';
-// import { DiagnosticoInicialModule } from './modules/diagnostico-inicial/diagnostico-inicial.module';
 import { HttpModule } from '@nestjs/axios';
 import { AnaliseMigracaoModule } from './modules/analise-migracao/analise-migracao.module';
 import { DiagnosticoInicialModule } from './modules/diagnostico-inicial/diagnostico-inicial.module';
--- server/src/modules/diagnostico-inicial/diagnostico-inicial.module.ts
+++ server/src/modules/diagnostico-inicial/diagnostico-inicial.module.ts
@@ -3,9 +3,10 @@ import { DiagnosticoInicialService } from './diagnostico-inicial.service';
 import { DiagnosticoInicialController } from './diagnostico-inicial.controller';
 import { HttpModule } from '@nestjs/axios';
 import { ReceitawsApiService } from 'src/integrations/receitaws-api/receitaws-api.service';
+import { AnaliseMigracaoModule } from '../analise-migracao/analise-migracao.module';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, AnaliseMigracaoModule],
   controllers: [DiagnosticoInicialController],
   providers: [DiagnosticoInicialService, ReceitawsApiService],
 })
