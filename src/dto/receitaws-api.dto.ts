91cced30c9ae99a5ed1f7beac337b7b0720b4f69
Luana Silva <luanasilva.developer@gmail.com>
2025-08-28 21:55:17 -0300

    remove: delete directory

--- src/dto/receitaws-api.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty, IsString, Length, Matches } from 'class-validator';
-
-export class ReceitawsApiDto {
-  @IsNotEmpty()
-  @IsString()
-  @Length(14, 14, { message: 'O CNPJ deve ter 14 dígitos' })
-  @Matches(/^\d+$/, { message: 'CNPJ deve conter apenas números' })
-  cnpj!: string;
-}
--- src/modules/diagnostico-inicial-treinamento/dto/update-diagnostico-inicial-treinamento.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateDiagnosticoInicialTreinamentoDto } from './create-diagnostico-inicial-treinamento.dto';
-
-export class UpdateDiagnosticoInicialTreinamentoDto extends PartialType(CreateDiagnosticoInicialTreinamentoDto) {}
