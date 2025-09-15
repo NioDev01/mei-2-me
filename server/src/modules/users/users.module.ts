9d541dd740f9f056d9f7280348e91cdf3232fab4
Luana Silva <luanasilva.developer@gmail.com>
2025-09-15 19:28:47 -0300

    feat: adiciona novos módulos

---
+++ server/src/modules/users/users.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { HttpModule } from '@nestjs/axios';
+import { ReceitawsApiService } from 'src/integrations/receitaws-api/receitaws-api.service';
+
+@Module({
+  imports: [HttpModule],
+  controllers: [UsersController],
+  providers: [UsersService, ReceitawsApiService],
+})
+export class UsersModule {}
