3bfece70deb050d3a781bd83bfb8c6d1aeef6baa
Luana Silva <luanasilva.developer@gmail.com>
2025-09-16 16:28:03 -0300

    feat: define regras para validar token

---
+++ server/src/modules/auth/jwt.strategy.ts
@@ -0,0 +1,35 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersService } from '../users/users.service';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    private usersService: UsersService,
+    private configService: ConfigService,
+  ) {
+    const secret = configService.get<string>('JWT_SECRET');
+
+    if (!secret) {
+      throw new Error('JWT_SECRET não definido no .env');
+    }
+
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: secret,
+    });
+  }
+
+  async validate(payload: { userCNPJ: string }) {
+    const user = await this.usersService.findOne(payload.userCNPJ);
+
+    if (!user) {
+      throw new UnauthorizedException('Usuário não encontrado.');
+    }
+
+    return user;
+  }
+}
