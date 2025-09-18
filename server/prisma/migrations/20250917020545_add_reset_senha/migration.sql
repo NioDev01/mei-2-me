c61e72457c04a71a81e83850538b516c433cfca3
Gabriel-93 <gabrielvduarte@gmail.com>
2025-09-17 21:30:09 -0300

    Removendo DTOs desnecessários e ajustes no schema conforme revisão

--- server/prisma/migrations/20250917020545_add_reset_senha/migration.sql
+++ server/prisma/migrations/20250917020545_add_reset_senha/migration.sql
@@ -1,12 +1,12 @@
 -- CreateTable
 CREATE TABLE "public"."ResetSenha" (
     "id" SERIAL NOT NULL,
-    "userId" INTEGER NOT NULL,
+    "id_user" INTEGER NOT NULL,
     "token" TEXT NOT NULL,
-    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "expira_em" TIMESTAMP(3) NOT NULL,
     "used" BOOLEAN NOT NULL DEFAULT false,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "criado_em" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "atualizado_em" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "ResetSenha_pkey" PRIMARY KEY ("id")
 );
@@ -15,4 +15,4 @@ CREATE TABLE "public"."ResetSenha" (
 CREATE UNIQUE INDEX "ResetSenha_token_key" ON "public"."ResetSenha"("token");
 
 -- AddForeignKey
-ALTER TABLE "public"."ResetSenha" ADD CONSTRAINT "ResetSenha_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."Usuario"("id_user") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "public"."ResetSenha" ADD CONSTRAINT "ResetSenha_userId_fkey" FOREIGN KEY ("id_user") REFERENCES "public"."Usuario"("id_user") ON DELETE RESTRICT ON UPDATE CASCADE;
--- server/prisma/reset-senha/dto/solicitar-reset.dto.ts
+++
@@ -1,6 +0,0 @@
-import { IsEmail } from 'class-validator';
-
-export class SolicitarResetDto {
-  @IsEmail()
-  email_user!: string;
-}
