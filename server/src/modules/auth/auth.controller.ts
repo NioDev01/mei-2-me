f40fbaa22b2966722749964acfa077abf7b44be0
Luana Silva <luanasilva.developer@gmail.com>
2025-09-25 19:20:19 -0300

    docs(api): documenta endpoints de autenticação

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Post, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthEntity } from './entity/auth.entity';
 import { LoginDto } from './dto/login.dto';
 import { ForgotPasswordDto } from './dto/email.dto';
@@ -11,12 +11,34 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  @ApiResponse({ type: AuthEntity })
+  @ApiOperation({
+    summary: 'Autentica usuário',
+    description:
+      'Endpoint responsável por realizar a autenticação de um usuário no sistema.',
+  })
+  @ApiResponse({
+    type: AuthEntity,
+    status: 200,
+    description: 'Usuário autenticado com sucesso!',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Não foi possível realizar a autenticação do usuário.',
+  })
   login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
   }
 
   @Post('forgot-password')
+  @ApiOperation({
+    summary: 'Solicita redefinição de senha.',
+    description:
+      'Endpoint responsável por fazer a solicitação de redefinição de senha.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Solicitação para redefinição de senha realizada com sucesso!',
+  })
   async forgotPassword(@Body() email: ForgotPasswordDto): Promise<void> {
     return this.authService.forgotPassword(email.email);
   }
