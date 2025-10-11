890f56c682e51dc2c7fe335ca3c44f6edd0c1e2c
Luana Silva <luanasilva.developer@gmail.com>
2025-10-11 17:02:21 -0300

    docs: cria documentação das rotas do módulo de checklist de documentos

--- server/src/modules/checklist-documentos/dto/create-checklist-documento.dto.ts
+++ server/src/modules/checklist-documentos/dto/create-checklist-documento.dto.ts
@@ -1,34 +1,43 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsNotEmpty, IsNumber } from 'class-validator';
 
 export class CreateChecklistDocumentoDto {
   @IsNumber()
+  @ApiProperty()
   id_mei!: number;
 
   @IsBoolean()
   @IsNotEmpty({ message: 'O usuário precisa selecionar uma opção.' })
+  @ApiProperty()
   possui_rg!: boolean;
 
   @IsBoolean()
   @IsNotEmpty({ message: 'O usuário precisa selecionar uma opção.' })
+  @ApiProperty()
   possui_cpf!: boolean;
 
   @IsBoolean()
   @IsNotEmpty({ message: 'O usuário precisa selecionar uma opção.' })
+  @ApiProperty()
   possui_cnh!: boolean;
 
   @IsBoolean()
   @IsNotEmpty({ message: 'O usuário precisa selecionar uma opção.' })
+  @ApiProperty()
   possui_ccmei!: boolean;
 
   @IsBoolean()
   @IsNotEmpty({ message: 'O usuário precisa selecionar uma opção.' })
+  @ApiProperty()
   possui_cartao_cnpj!: boolean;
 
   @IsBoolean()
   @IsNotEmpty({ message: 'O usuário precisa selecionar uma opção.' })
+  @ApiProperty()
   possui_insc_estadual!: boolean;
 
   @IsBoolean()
   @IsNotEmpty({ message: 'O usuário precisa selecionar uma opção.' })
+  @ApiProperty()
   possui_insc_municipal!: boolean;
 }
