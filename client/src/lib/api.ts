46ad1a4f827e6042257514ec21bc979b0bda623a
Antonio Sales <antoniosalesdev@gmail.com>
2026-04-07 01:08:44 -0300

    fix(api.ts): update baseURL to use environment variable for API URL

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAccessToken, setAccessToken } from "./auth";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
