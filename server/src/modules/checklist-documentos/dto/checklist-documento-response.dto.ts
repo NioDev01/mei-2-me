9eeee90d48073a13efae45ecdfe351a76e259f47
Luana Silva <luanasilva.developer@gmail.com>
2026-04-19 20:42:14 -0300

    feat: create response checklist dto

--- server/src/modules/checklist-documentos/dto/checklist-documento-response.dto.ts
+++ server/src/modules/checklist-documentos/dto/checklist-documento-response.dto.ts
@@ -1,7 +1,7 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsBoolean, IsOptional } from 'class-validator';
 
-export class CreateChecklistDocumentoDto {
+export class ChecklistDocumentoResponseDto {
   @IsBoolean()
   @IsOptional()
   @ApiPropertyOptional()
